110dbc8bc439d93fb2875e851c4c41b89809cc7d
JuqiXie <JuqiXie@192.168.1.101>
2016-06-09 15:23:18 +0800

    Quartz的另一种方式

--- src/com/baobaotao/quartz/SimpleTriggerRunner.java
+++ src/com/baobaotao/quartz/SimpleTriggerRunner.java
@@ -54,4 +54,35 @@ public class SimpleTriggerRunner {
 		scheduler.start();
 		
 	}
+	
+	
+	private static void method2() throws SchedulerException{
+		/**1.创建一个JobDetail实例，指定SimpleJob*/
+		JobDetail jobDetail = new JobDetail("job1_1", "jgroup1", SimpleJob.class);
+
+		/**2.通过SimpleTrigger定义调度规则，马上启动，每2秒运行一次，共运行100次*/
+		SimpleTrigger simpleTrigger = new SimpleTrigger("trigger1_1","tgroup1");
+		simpleTrigger.setStartTime(new Date());
+		simpleTrigger.setRepeatInterval(2000);
+		simpleTrigger.setRepeatCount(100);
+		
+		
+		/**2.1指定关联的Job组名*/
+		simpleTrigger.setJobGroup("jgroup1");
+		/**2.2指定关联的Job名称*/
+		simpleTrigger.setJobName("job1_1");
+		
+		/**3.通过SchedulerFactory获取一个调度器实例*/
+		SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+		Scheduler scheduler = schedulerFactory.getScheduler();
+		
+		/**3.1注册JobDetail*/
+		scheduler.addJob(jobDetail, true);
+		
+		/**4.注册并进行调度*/
+		scheduler.scheduleJob(jobDetail,simpleTrigger);
+		
+		/**5.调度启动*/
+		scheduler.start();
+	}
 }
